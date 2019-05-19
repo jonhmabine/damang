4dafaaba89830ed14d9d176d8206c4e7852ff84b
jonhmabine <50687353+jonhmabine@users.noreply.github.com>
2019-05-19 21:55:41 +0800

    Add files via upload

---
+++ friend.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+clear
+pkg install update
+pkg install upgrade
+apt update
+apt upgrade -y
+pkg install php -y
+
+echo
+echo -e "\e[1m\e[32m Subscribe :\e[33m Termux Tricks & Tut"
+echo
+echo -e "\e[1m\e[32m Enter :\e[33m php fw.php"
+echo
